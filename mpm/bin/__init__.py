4c0037608247fa533ba9ab40de311d2d66eb6171
Christian Fobel <christian@fobel.net>
2016-03-09 09:44:19 -0500

    [NB] Implement freeze/install/uninstall commands
    Implement freeze/install/uninstall commands.
    
    Usage
    =====
    
        mpm [-h] [-l {error,debug,info}]
            [-c CONFIG_FILE | -d PLUGINS_DIRECTORY]
            {install,uninstall,freeze} ...
    
        Microdrop plugin manager
    
        positional arguments:
          {install,uninstall,freeze}
                                help for subcommand
            install             Install plugins.
            uninstall           Uninstall plugins.
            freeze              Output installed packages in requirements format.
    
        optional arguments:
          -h, --help            show this help message and exit
          -l {error,debug,info}, --log-level {error,debug,info}
          -c CONFIG_FILE, --config-file CONFIG_FILE
                                Microdrop config file (default="C:\Users\Christian\Doc
                                uments\Microdrop\microdrop.ini").
          -d PLUGINS_DIRECTORY, --plugins-directory PLUGINS_DIRECTORY
                                Microdrop plugins directory (default="C:\Users\Christi
                                an\Documents\Microdrop\plugins").
    
    `install` usage
    ===============
    
        mpm install [-h] [-s SERVER_URL] plugin [plugin ...]
    
        positional arguments:
          plugin
    
        optional arguments:
          -h, --help            show this help message and exit
          -s SERVER_URL, --server-url SERVER_URL
                                Microdrop plugin index URL
                                (default="http://microfluidics.utoronto.ca/update")
    
    `uninstall` usage
    =================
    
        mpm uninstall [-h] plugin [plugin ...]
    
        positional arguments:
          plugin
    
        optional arguments:
          -h, --help  show this help message and exit
    
    `freeze` usage
    ==============
    
        mpm freeze [-h]
    
        optional arguments:
          -h, --help  show this help message and exit

--- mpm/bin/__init__.py
+++ mpm/bin/__init__.py
@@ -1,24 +1,12 @@
 from argparse import ArgumentParser
-from pip_helpers import CRE_PACKAGE
-import os
+from path_helpers import path
+import logging
 import sys
 
+from ..commands import (DEFAULT_INDEX_HOST, freeze, get_plugins_directory,
+                        install, SERVER_URL_TEMPLATE, uninstall)
 
-def home_dir():
-    if os.name == 'nt':
-        from win32com.shell import shell, shellcon
-
-        return shell.SHGetFolderPath(0, shellcon.CSIDL_PERSONAL, 0, 0)
-    else:
-        return os.path.expanduser('~')
-
-
-def plugin_request(plugin_str):
-    match = CRE_PACKAGE.match(plugin_str)
-    if not match:
-        raise ValueError('Invalid plugin descriptor. Must be like "foo", '
-                         '"foo==1.0", "foo>=1.0", etc.')
-    return match.groupdict()
+logger = logging.getLogger(__name__)
 
 
 def parse_args(args=None):
@@ -27,7 +15,28 @@ def parse_args(args=None):
     if args is None:
         args = sys.argv
 
+    default_plugins_directory = get_plugins_directory()
+    default_config_path = (default_plugins_directory.parent
+                           .joinpath('microdrop.ini'))
+
     parser = ArgumentParser(description='Microdrop plugin manager')
+    parser.add_argument('-l', '--log-level', choices=['error', 'debug',
+                                                      'info'],
+                        default='error')
+    mutex_path = parser.add_mutually_exclusive_group()
+    mutex_path.add_argument('-c', '--config-file', type=path,
+                            help='Microdrop config file '
+                            '(default="{default}").'
+                            .format(default=default_config_path))
+    mutex_path.add_argument('-d', '--plugins-directory', type=path,
+                            help='Microdrop plugins directory '
+                            '(default="{default}").'
+                            .format(default=default_plugins_directory))
+
+    server_parser = ArgumentParser(add_help=False)
+    server_parser.add_argument('-s', '--server-url',
+                               default=DEFAULT_INDEX_HOST, help='Microdrop '
+                               'plugin index URL (default="%(default)s")')
 
     plugins_parser = ArgumentParser(add_help=False)
     plugins_parser.add_argument('plugin', nargs='+')
@@ -36,7 +45,8 @@ def parse_args(args=None):
                                        dest='command')
 
     parser_install = subparsers.add_parser('install', help='Install plugins.',
-                                           parents=[plugins_parser])
+                                           parents=[plugins_parser,
+                                                    server_parser])
 
     parser_uninstall = subparsers.add_parser('uninstall',
                                              help='Uninstall plugins.',
@@ -46,11 +56,42 @@ def parse_args(args=None):
                                           'packages in requirements format.')
 
     args = parser.parse_args()
-    if hasattr(args, 'plugin'):
-        args.plugin = map(plugin_request, args.plugin)
+
+    logging.basicConfig(level=getattr(logging, args.log_level.upper()))
+    if hasattr(args, 'server_url'):
+        logger.debug('Using Microdrop index server: "%s"', args.server_url)
+        args.server_url = SERVER_URL_TEMPLATE % args.server_url
+    if all([args.plugins_directory is None,
+            args.config_file is None]):
+        args.plugins_directory = default_plugins_directory
+        logger.debug('Using default plugins directory: "%s"',
+                     args.plugins_directory)
+    elif args.plugins_directory is None:
+        args.plugins_directory = get_plugins_directory(config_path=
+                                                       args.config_file)
+        logger.debug('Plugins directory from config file: "%s"',
+                     args.plugins_directory)
+    else:
+        logger.debug('Using explicit plugins directory: "%s"',
+                     args.plugins_directory)
     return args
 
 
 def main():
     args = parse_args()
-    print args
+    logger.debug('Arguments: %s', args)
+    if args.command == 'freeze':
+        print '\n'.join(freeze(plugins_directory=args.plugins_directory))
+    elif args.command == 'install':
+        for plugin_i in args.plugin:
+            try:
+                install(plugin_package=plugin_i,
+                        plugins_directory=args.plugins_directory,
+                        server_url=args.server_url)
+            except ValueError, exception:
+                print exception.message
+                continue
+    elif args.command == 'uninstall':
+        for plugin_i in args.plugin:
+            uninstall(plugin_package=plugin_i,
+                      plugins_directory=args.plugins_directory)
