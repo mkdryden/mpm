1107c333a8058f873d00147c9387274e6953c97a
Christian Fobel <christian@fobel.net>
2016-08-09 13:53:04 -0400

    Refactor arg parse to share setup code

--- mpm/bin/__init__.py
+++ mpm/bin/__init__.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 from argparse import ArgumentParser
 from path_helpers import path
 import logging
@@ -8,55 +9,72 @@ from ..commands import (DEFAULT_INDEX_HOST, freeze, get_plugins_directory,
 
 logger = logging.getLogger(__name__)
 
+default_plugins_directory = get_plugins_directory()
+default_config_path = (default_plugins_directory.parent
+                       .joinpath('microdrop.ini'))
+
+# Parsers that may be reused by other modules.
+LOG_PARSER = ArgumentParser(add_help=False)
+LOG_PARSER.add_argument('-l', '--log-level', default='error',
+                        choices=['error', 'debug', 'info'])
+CONFIG_PARSER_ARGS = (('-c', '--config-file'),
+                      dict(type=path, help='Microdrop config file '
+                           '(default="{default}").'
+                           .format(default=default_config_path)))
+CONFIG_PARSER = ArgumentParser(add_help=False)
+CONFIG_PARSER.add_argument(*CONFIG_PARSER_ARGS[0], **CONFIG_PARSER_ARGS[1])
+
+SERVER_PARSER = ArgumentParser(add_help=False)
+SERVER_PARSER.add_argument('-s', '--server-url',
+                            default=DEFAULT_INDEX_HOST, help='Microdrop '
+                            'plugin index URL (default="%(default)s")')
+PLUGINS_PARSER = ArgumentParser(add_help=False)
+PLUGINS_PARSER.add_argument('plugin', nargs='+')
+
+MPM_PARSER = ArgumentParser(add_help=False, parents=[LOG_PARSER])
+mutex_path = MPM_PARSER.add_mutually_exclusive_group()
+mutex_path.add_argument(*CONFIG_PARSER_ARGS[0], **CONFIG_PARSER_ARGS[1])
+mutex_path.add_argument('-d', '--plugins-directory', type=path,
+                        help='Microdrop plugins directory '
+                        '(default="{default}").'
+                        .format(default=default_plugins_directory))
+subparsers = MPM_PARSER.add_subparsers(help='help for subcommand',
+                                       dest='command')
+subparsers.add_parser('install', help='Install plugins.',
+                      parents=[PLUGINS_PARSER, SERVER_PARSER])
+subparsers.add_parser('uninstall', help='Uninstall plugins.',
+                      parents=[PLUGINS_PARSER])
+subparsers.add_parser('freeze', help='Output installed packages in '
+                      'requirements format.')
+
 
 def parse_args(args=None):
     '''Parses arguments, returns (options, args).'''
-
     if args is None:
         args = sys.argv
 
-    default_plugins_directory = get_plugins_directory()
-    default_config_path = (default_plugins_directory.parent
-                           .joinpath('microdrop.ini'))
+    parser = ArgumentParser(description='Microdrop plugin manager',
+                            parents=[MPM_PARSER])
 
-    parser = ArgumentParser(description='Microdrop plugin manager')
-    parser.add_argument('-l', '--log-level', choices=['error', 'debug',
-                                                      'info'],
-                        default='error')
-    mutex_path = parser.add_mutually_exclusive_group()
-    mutex_path.add_argument('-c', '--config-file', type=path,
-                            help='Microdrop config file '
-                            '(default="{default}").'
-                            .format(default=default_config_path))
-    mutex_path.add_argument('-d', '--plugins-directory', type=path,
-                            help='Microdrop plugins directory '
-                            '(default="{default}").'
-                            .format(default=default_plugins_directory))
+    return parser.parse_args()
 
-    server_parser = ArgumentParser(add_help=False)
-    server_parser.add_argument('-s', '--server-url',
-                               default=DEFAULT_INDEX_HOST, help='Microdrop '
-                               'plugin index URL (default="%(default)s")')
 
-    plugins_parser = ArgumentParser(add_help=False)
-    plugins_parser.add_argument('plugin', nargs='+')
+def validate_args(args):
+    '''
+    Apply custom validation and actions based on parsed arguments.
 
-    subparsers = parser.add_subparsers(help='help for subcommand',
-                                       dest='command')
+    Args
+    ----
 
-    parser_install = subparsers.add_parser('install', help='Install plugins.',
-                                           parents=[plugins_parser,
-                                                    server_parser])
+        args (Namespace) : Result from `parse_args` method of
+            `argparse.ArgumentParser` instance.
 
-    parser_uninstall = subparsers.add_parser('uninstall',
-                                             help='Uninstall plugins.',
-                                             parents=[plugins_parser])
+    Returns
+    -------
 
-    parser_freeze = subparsers.add_parser('freeze', help='Output installed '
-                                          'packages in requirements format.')
-
-    args = parser.parse_args()
-
+        (Namespace) : Reference to input `args`, which have been
+            validated/updated.
+    '''
     logging.basicConfig(level=getattr(logging, args.log_level.upper()))
     if hasattr(args, 'server_url'):
         logger.debug('Using Microdrop index server: "%s"', args.server_url)
@@ -77,8 +95,10 @@ def parse_args(args=None):
     return args
 
 
-def main():
-    args = parse_args()
+def main(args=None):
+    if args is None:
+        args = parse_args()
+    args = validate_args(args)
     logger.debug('Arguments: %s', args)
     if args.command == 'freeze':
         print '\n'.join(freeze(plugins_directory=args.plugins_directory))
